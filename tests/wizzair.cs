1e18d48482d36b01fd5c0eb2011be4628dd438a4
Adrian Dolniak <adolniak@objectivity.co.uk>
2021-04-12 12:49:46 +0200

    minor fixes

--- tests/wizzair.cs
+++ tests/wizzair.cs
@@ -3,11 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 
 namespace SeleniumCSharp.tests
@@ -20,7 +16,7 @@ namespace SeleniumCSharp.tests
         [SetUp]
             public void StartBrowser()
         {
-            driver = new ChromeDriver("C:\\Users\\Adrian\\source\\chromedriver_win32\\");
+            driver = new ChromeDriver("D:\\Testing\\Visual Studio C # Testing\\");
             driver.Url = "https://www.wizzair.com/en-en#/";
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
@@ -37,7 +33,7 @@ namespace SeleniumCSharp.tests
         {
             IWebElement signIn = driver.FindElement(By.XPath("//*[@id='app']/div/header/div[1]/div/nav/ul/li[7]/button"));
             signIn.Click();
-            IWebElement registration = driver.FindElement(By.XPath("//*[@id='login-modal']/form/div/p/button"));
+            IWebElement registration = driver.FindElement(By.XPath("//p//button[@class='content__link1']"));
             registration.Click();
             IWebElement fName = driver.FindElement(By.XPath("//*[@id='regmodal-scroll-hook-1']/label[1]/div[1]/input"));
             fName.SendKeys("Jan");
@@ -59,13 +55,14 @@ namespace SeleniumCSharp.tests
             IWebElement password = driver.FindElement(By.XPath("//*[@id='regmodal-scroll-hook-5']/div[1]/label/input"));
             password.SendKeys("QWERTYuiop12345");
             IWebElement nationality = driver.FindElement(By.XPath("//*[@id='regmodal-scroll-hook-6']/div[1]/label/input"));
-            nationality.SendKeys("Polska");
-            IWebElement checkBox1 = driver.FindElement(By.XPath("//*[@id='registration-modal']/form/div[2]/div[9]/span/label[1]"));
+            nationality.SendKeys("Poland");
+            IWebElement checkBox1 = driver.FindElement(By.XPath("//label[@for='registration-special-offers-checkbox']"));
             Boolean status1 = checkBox1.Selected;
             Assert.IsFalse(status1);
-            IWebElement checkBox2 = driver.FindElement(By.XPath("//*[@id='registration-modal']/form/div[2]/div[10]/span/label[1]"));
+            IWebElement checkBox2 = driver.FindElement(By.XPath("//label[@for='registration-privacy-policy-checkbox']"));
             Boolean status2 = checkBox2.Selected;
             Assert.IsFalse(status2);
+            //właściwy test
             IWebElement verify = driver.FindElement(By.XPath("//*[@id='regmodal-scroll-hook-4']/div[2]/span/span"));
             Boolean status3 = verify.Displayed;
             Assert.IsTrue(status3);
